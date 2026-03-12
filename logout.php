8645efe448d9508ac60bde34d6438a5bffefdc8d
Rogério <141028237+master-rogerio@users.noreply.github.com>
2026-03-12 11:31:12 -0300

    Update logout.php
    Atualização para logout completo.

--- logout.php
+++ logout.php
@@ -6,6 +6,20 @@
  * ============================================================================
  */
 session_start();
+
+//Limpa todas as variáveis da memória do servidor 
+$_SESSION = array();
+
+// Destrói o cookie de sessão no navegador do cliente.
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000, 
+        $params["path"], $params["domain"], 
+        $params["secure"], $params["httponly"]
+    );
+}
+
+//Destrói a sessão no servidor
 session_destroy();
 header('Location: index.html');
 exit;
